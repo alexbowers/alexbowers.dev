0831ee42561e1f5e834a67aad5ceaf1feace93af
Alex Bowers <bowersbros@gmail.com>
2019-03-01 00:11:32 +0000

    Initial Commit

---
+++ dist/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html class="h-full w-full bg-indigo-darker texture">
+    <head>
+        <title>Alex Bowers - Software Developer</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body class="font-sans w-full h-full overflow-x-hidden flex flex-col">
+        <div class="absolute pin-x">
+            <div class="bg-indigo-darkest w-64 h-64 shadow-lg opacity-50 skew-30-and-zoom-250 absolute pin-l paper"></div>
+            <div class="bg-indigo-darkest w-64 h-64 shadow-lg skew-45-and-zoom-250 absolute pin-t paper" style="background-color: #36354D"></div>
+        </div>
+
+        <div class="absolute pin-r">
+            <div class="bg-indigo-dark w-64 h-64 shadow-lg skew-45-and-zoom-250 paper"></div>
+            <div class="bg-indigo-darkest w-64 h-64 shadow-lg skew-60-and-zoom-250 paper" style="background-color: #36354D"></div>
+            <div class="bg-indigo-darker w-64 h-64 shadow-lg skew-20-and-zoom-250 paper"></div>
+        </div>
+
+        <div class="absolute w-full">
+            <div class="w-full mx-auto sm:px-8 lg:px-12 xl:px-24 lg:max-w-2lg xl:max-w-3xl">
+                <header class="w-full py-8 text-center sm:pt-16 sm:pb-12 flex items-end justify-between">
+                    <a href class="inline-flex items-baseline antialiased text-2xl no-underline group">
+                        <div class="text-grey group-hover:text-grey-dark mr-1">Alex</div>
+                        <div class="text-red-light group-hover:text-red-dark">Bowers</div>
+                    </a>
+
+                    <nav class="inline-flex items-baseline font-medium antialiased mt-4 sm:mt-0">
+                        <a class="text-red-light hover:text-red-lighter no-underline ml-4 md:ml-8" href="https://github.com/alexbowers">GitHub</a>
+                        <a class="text-red-light hover:text-red-lighter no-underline ml-4 md:ml-8" href="https://twitter.com/bowersbros">Twitter</a>
+                    </nav>
+                </header>
+            </div>
+        </div>
+    </body>
+</html>
